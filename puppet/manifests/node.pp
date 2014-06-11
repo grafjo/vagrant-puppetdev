5e72fa8344d8431eb8df3d6c71e70b5d81031c34
Johannes Graf <graf@synyx.de>
2014-06-11 23:41:37 +0200

    Updated to graylog2 0.20.2

--- puppet/manifests/node.pp
+++ puppet/manifests/node.pp
@@ -33,9 +33,12 @@ node 'vagrant.example.com' {
     root_username      => 'admin',
     root_password_sha2 => '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', # admin
     password_secret    => 'You MUST set a secret to secure/pepper the stored user passwords here. Use at least 64 characters',
+    rest_listen_uri    => 'http://192.168.33.10:12900/',
+    rest_transport_uri => 'http://192.168.33.10:12900/',
   }->
   class { 'graylog2::web':
-    application_secret => 'You MUST set a secret to secure/pepper the stored user passwords here. Use at least 64 characters',
+    application_secret   => 'You MUST set a secret to secure/pepper the stored user passwords here. Use at least 64 characters',
+    graylog2_server_uris => ["http://192.168.33.10:12900/"],
   }
 
 }
