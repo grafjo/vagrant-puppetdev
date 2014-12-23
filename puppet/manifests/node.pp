fcb0d42c3919feb1dd8f6e4bd8046f91ad4726eb
Johannes Graf <graf@synyx.de>
2014-12-23 23:56:32 +0100

    Demo box for https://github.com/Graylog2/graylog2-puppet/issues/13

--- puppet/manifests/node.pp
+++ puppet/manifests/node.pp
@@ -35,19 +35,21 @@ node 'vagrant.example.com' {
     ],
   }
 
-  class {'graylog2::repo':}->
+ class { 'graylog2::repo':
+    version => '0.92'
+  } ->
   class { 'graylog2::server':
-    root_username      => 'admin',
-    root_password_sha2 => '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', # admin
-    password_secret    => 'You MUST set a secret to secure/pepper the stored user passwords here. Use at least 64 characters',
-    rest_listen_uri    => 'http://192.168.33.10:12900/',
-    rest_transport_uri => 'http://192.168.33.10:12900/',
-  }->
+    password_secret            => '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
+    root_password_sha2         => '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
+    elasticsearch_cluster_name => 'graylog2',
+    require                    => [
+      Class['graylog2::repo'],
+    ],
+  } ->
   class { 'graylog2::web':
-    application_secret   => 'You MUST set a secret to secure/pepper the stored user passwords here. Use at least 64 characters',
-    graylog2_server_uris => ["http://192.168.33.10:12900/"],
+    application_secret => '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
+    require            => Class['graylog2::server'],
   }
-
 
   class { 'apache':
     default_vhost     => false,
